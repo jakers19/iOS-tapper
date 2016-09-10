261c7494ca3d95746908bc2c89a5c3728a7ac28f
jakers19 <jakers19@gmail.com>
2016-09-09 22:21:26 -0500

    wired up, made universal for ipad. final

--- ios-tapper/ViewController.swift
+++ ios-tapper/ViewController.swift
@@ -9,7 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    //properties
+    var maxTaps = 0
+    var currentTaps = 0
+    
+    //outlets
+    
     @IBOutlet weak var logoImg: UIImageView!
     @IBOutlet weak var howManyTapsTxt: UITextField!
     @IBOutlet weak var playBtn: UIButton!
@@ -17,16 +22,62 @@ class ViewController: UIViewController {
     @IBOutlet weak var tapBtn: UIButton!
     @IBOutlet weak var tapsLbl: UILabel!
     
-    @IBAction func onPlayBtnPressed(sender: UIButton!) {
-        logoImg.hidden = true
-        playBtn.hidden = true
-        howManyTapsTxt.hidden = true
+    // IBAction Functions
+    // tapping the coin to increase the number until you hit the designated maximum, if max hit, start game over
+    
+    @IBAction func onCoinTapped(sender: UIButton!) {
+        currentTaps += 1
+        updateTapsLbl()
         
-        tapBtn.hidden = false
-        tapsLbl.hidden = false
+        if isGameOver() {
+            restartGame()
+        }
+    }
+    // start the game
+    
+    @IBAction func onPlayBtnPressed(sender: UIButton!) {
+        
+        if howManyTapsTxt.text != nil && howManyTapsTxt.text != "" {
+            logoImg.hidden = true
+            playBtn.hidden = true
+            howManyTapsTxt.hidden = true
+            
+            tapBtn.hidden = false
+            tapsLbl.hidden = false
+            
+            maxTaps = Int(howManyTapsTxt.text!)!
+            currentTaps = 0
+            
+            updateTapsLbl()
+        }
     }
     
-
-
+    // counter label
+    
+    func updateTapsLbl() {
+        tapsLbl.text = "\(currentTaps) Taps"
+    }
+    // check to see if you've hit max
+    
+    func isGameOver() -> Bool {
+        if currentTaps >= maxTaps {
+            return true
+        } else {
+            return false
+        }
+    }
+     // go back to title screen if game is over
+    
+        func restartGame() {
+            maxTaps = 0
+            howManyTapsTxt.text = ""
+            
+            logoImg.hidden = false
+            playBtn.hidden = false
+            howManyTapsTxt.hidden = false
+            
+            tapBtn.hidden = true
+            tapsLbl.hidden = true
+        }
 }
 
